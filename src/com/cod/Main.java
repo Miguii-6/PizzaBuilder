6aa029ed8259b533ae1792bf27b9ade5b8d254bf
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-06-18 21:27:16 +0200

    Main con las pizzas creadas

--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -3,19 +3,20 @@ package com.cod;
 public class Main {
 
     public static void main(String[] args) {
+        Pizzas piz1 = new BuilderPizzas().build();
+        System.out.println(piz1.toString());
 
-	    // utilizamos el builder y en concreto el metodo build
-        // para tener nuestra pizza
-	    Pizzas creadaConBuilder = new BuilderPizzas().build();
-	    Pizzas sintaxisEncadenada = new BuilderPizzas()
-                .setCebolla(true)
-                .setSize(Pizzas.BIG)
+        Pizzas pizPinha = new BuilderPizzas().setPinha(true).build();
+        System.out.println(pizPinha.toString()+("Para la gente sin criterio"));
+
+        Pizzas pizEncadenada = new BuilderPizzas()
+                .setBacon(false)
+                .setSize(3)
+                .setTipoMasa(0)
                 .build();
+        System.out.println(pizEncadenada.toString());
 
-	    // aunque el toSpring no está definida, nos valen estas lineas para poner
-        // un punto de ruptura y hacer debug
-        // asi podemos ver los objetos creados
-        System.out.println(creadaConBuilder.toString());
-        System.out.println(sintaxisEncadenada.toString());
+
+
     }
 }
